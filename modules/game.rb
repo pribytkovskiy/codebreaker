53055a9ee339806ac4b2b15837d0b773a61fc011
Dmitriy <dr.trem86@gmail.com>
2019-01-11 16:03:05 +0200

    end fix review

--- modules/game.rb
+++ modules/game.rb
@@ -50,33 +50,7 @@ module Codebreaker
     end
 
     def mark
-      mark_plus
-      mark_minus
-    end
-
-    def mark_plus
-      @array_code = Array.new(@array_input_code)
-      @array_secret_code = Array.new(secret_code)
-      @answer = []
-      @array_code.zip(@array_secret_code).each_with_index do |(code, secret_code), i|
-        next if code != secret_code
-
-        @array_secret_code[i], @array_code[i] = nil
-        @answer << '+'
-      end
-    end
-
-    def mark_minus
-      @array_code.compact!
-      @array_secret_code.compact!
-      @array_code.map do |code|
-        index = code && @array_secret_code.index(code)
-        next unless index
-
-        @array_secret_code[index] = nil
-        @answer << '-'
-      end
-      @answer.join if game_status == STATUS[:game]
+      Codebreaker::Mark.mark_plus(@array_input_code, secret_code)
     end
   end
 end
--- modules/mark.rb
+++ modules/mark.rb
@@ -1,36 +1,32 @@
 module Codebreaker
   class Mark
     class << self
-      def mark(game)
-        mark_plus(array_input_code, secret_code)
-        mark_minus
+      def mark_plus(input_code, secret_code)
+        array_code = Array.new(input_code)
+        array_secret_code = Array.new(secret_code)
+        @answer = []
+        array_code.zip(array_secret_code).each_with_index do |(code, secret_code), i|
+          next if code != secret_code
+
+          array_secret_code[i], array_code[i] = nil
+          @answer << '+'
+        end
+        mark_minus(array_code, array_secret_code)
       end
 
       private
 
-      def mark_plus
-        array_code = Array.new(game.array_input_code)
-        array_secret_code = Array.new(game.secret_code)
-        @answer = []
-        @array_code.zip(@array_secret_code).each_with_index do |(code, secret_code), i|
-          next if code != secret_code
-
-          @array_secret_code[i], @array_code[i] = nil
-          @answer << '+'
-        end
-      end
-
-      def mark_minus
-        @array_code.compact!
-        @array_secret_code.compact!
-        @array_code.map do |code|
-          index = code && @array_secret_code.index(code)
+      def mark_minus(array_code, array_secret_code)
+        array_code.compact!
+        array_secret_code.compact!
+        array_code.map do |code|
+          index = code && array_secret_code.index(code)
           next unless index
 
-          @array_secret_code[index] = nil
+          array_secret_code[index] = nil
           @answer << '-'
         end
-        @answer.join if game_status == :game
+        @answer.join
       end
     end
   end
